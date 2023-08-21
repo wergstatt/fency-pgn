f2bced1f24118f2dfcb80b57b141c3105908f050
jawerg <jan.wergula@posteo.de>
2023-08-21 10:16:18 +0200

    [py] add pymodule that derives fens from moves

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,23 @@
 pub mod utils;
+
+use crate::utils::game::Game;
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pymodule]
+fn fency_pgn(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(fentasize, m)?)?;
+    Ok(())
+}
+
+#[pyfunction]
+fn fentasize(moves: Vec<&str>) -> PyResult<Vec<String>> {
+    let mut game = Game::new();
+    let fens: Vec<String> = moves.iter().fold(Vec::new(), |mut acc, &mv| {
+        game.play_move(mv);
+        acc.push(game.clone().to_fen());
+        acc
+    });
+
+    Ok(fens)
+}
